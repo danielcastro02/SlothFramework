8d107d0c01bf907c57bc16d54c5b2ab9e2d398e5
Daniel <Daniel@200.132.17.123>
2019-05-31 13:27:50 -0300

    Inicio Gerador PDO

--- Controle/geradorPDO.php
+++ Controle/geradorPDO.php
@@ -40,7 +40,7 @@ class geradorPDO {
         $regras = $semente->getRegra();
         $preSql = "create table if not exists ".$semente->getNome()." (\n";
         $q = (count($att)-1);
-        for ($i = 0; $i < (count($att) - 1); $i++) {
+        for ($i = 0; $i < $q; $i++) {
             $preSql = $preSql .  $att[$i] . " " . $tipos[$i] . " " . $regras[$i] . " ,\n";
         }
         $preSql = $preSql .  $att[$q] . " " . $tipos[$q] . " " . $regras[$q] . "\n);";
@@ -91,9 +91,72 @@ class geradorPDO {
             $conteudo = $conteudo. "}";
             
             file_put_contents("../Modelo/". ucfirst($semente->getNome()).".php", $conteudo);
-            
+            $this->gerarPDO($semente);
         }else{
+            header('location: ../index.php?msg=ERRO');
         }
+    }
+    
+    public function gerarPDO(gerador $semente){
+        $nome = ucfirst($semente->getNome());
+        $nomeNormal = $semente->getNome();
+        $atributos = $semente->getAtributo();
+        $regras = $semente->getRegra();
+        $tipos = $semente->getTipo();
+        $conteudo = "
+<?php
+
+if (realpath('./index.php')) {
+    include_once './Controle/conexao.php';
+    include_once './Modelo/".$nome.".php';
+} else {
+    if (realpath('../index.php')) {
+        include_once '../Controle/conexao.php';
+        include_once '../Modelo/".$nome.".php';
+    } else {
+        if (realpath('../../index.php')) {
+            include_once '../../Controle/conexao.php';
+            include_once '../../Modelo/".$nome.".php';
+        }
+    }
+}
+
+
+class ".$nome."{
+    function inserir".$nome."() {
+        \$".$nomeNormal." = new ".$nomeNormal."(\$_POST);
+        \$con = new conexao();
+        \$pdo = \$con->getConexao();
+        \$stmt = \$pdo->prepare('insert into ".$nome." values(";
+        
+        $buscaRegra = explode(" ", $semente->getRegra()[0]);
+        if(in_array("auto_increment", $buscaRegra)||in_array("AUTO_INCREMENT", $buscaRegra)){
+            $conteudo = $conteudo."default , ";
+        }else{
+            $conteudo = $conteudo.":".$semente->getAtributo()[0]." , ";
+        }
+        
+        for($i = 1; $i<count($atributos); $i++){
+            $conteudo = $conteudo.":".$atributos[$i]." , ";
+        }
+        $conteudo = $conteudo.");\n";
+        for($i = 0; $i<count($atributos); $i++){
+        $conteudo = $conteudo."
+        \$stmt->bindValue(':".$atributo[$i]."', \$".$nome."->get".ucfirst($atributo[$i])."());    
+                ";
+        }
+                
+        $stmt->bindValue(':nome', $contato->getNome());
+        $stmt->bindValue(':cpf', $contato->getCpfCnpj());
+        $stmt->bindValue(':email', $contato->getEmail());
+        $stmt->bindValue(':descricao', $contato->getDescricao());
+        if ($stmt->execute()) {
+            header('location: ../Tela/Sistema/reclamacao.php?msg=sucessoReclamacao');
+        } else {
+            header('location: ../Tela/Sistema/reclamacao.php?msg=erroReclamacao');
+        }
+    }
+";
     }
 
 }
