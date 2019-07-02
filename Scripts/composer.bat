4069d9f013248e4963f16bb5e9e4d5ce1948c691
Daniel <Daniel@200.132.17.123>
2019-07-01 22:12:30 -0300

    Correção erro relatório

--- Scripts/composer.bat
+++ Scripts/composer.bat
@@ -1,5 +1,5 @@
 cd ..
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+c:\xampp\php\php.exe -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+c:\xampp\php\php.exe -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+c:\xampp\php\php.exe composer-setup.php
+c:\xampp\php\php.exe -r "unlink('composer-setup.php');"
