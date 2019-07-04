45216a93026bdaabcf330bac020f2bb3d9bd9009
Daniel <Daniel@200.132.17.123>
2019-07-04 17:28:00 -0300

    Correção erro relatório

--- Controle/autoDelete.php
+++ Controle/autoDelete.php
@@ -6,6 +6,33 @@ foreach ($vetor as $arquivo){
     $arquivo2 = trim($arquivo);
     unlink($arquivo2);
 }
-file_put_contents("../index.php", "<h1>HelloWorld</h1>");
+file_put_contents("../index.php", "<!DOCTYPE html>
+<?php
+if (isset(\$_SESSION['logado'])) {
+    header('location: ../Tela/home.php');
+}
+?>
+
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>Login</title>
+        <?php
+        include_once './Base/header.php';
+        ?>
+    <body class=\"homeimg\">
+        <?php
+        include_once './Base/navBar.php';
+        ?>
+        <main>
+           
+        </main>
+        <?php
+        include_once './Base/footer.php';
+        ?>
+    </body>
+</html>
+
+");
 rmdir("../Scripts");
 header("location: ../index.php");
