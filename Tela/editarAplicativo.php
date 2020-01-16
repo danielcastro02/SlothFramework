ef2f411ed8c6cd39ca45506798ae16fbdbef2d2c
Anesi <daniel.o.anesi@gmail.com>
2020-01-16 17:09:49 -0300

    Editando e exluindo app

--- Tela/editarAplicativo.php
+++ Tela/editarAplicativo.php
@@ -8,6 +8,14 @@ include_once '../Base/requerLogin.php';
         <title>Login</title>
         <?php
         include_once '../Base/header.php';
+        include_once '../Controle/clientePDO.php';
+        include_once '../Controle/projetoPDO.php';
+        include_once '../Controle/sitePDO.php';
+        include_once '../Controle/versaoPDO.php';
+        include_once '../Modelo/Cliente.php';
+        include_once '../Modelo/Site.php';
+        include_once '../Modelo/Projeto.php';
+        include_once '../Modelo/Versao.php';
         ?>
     <body class="homeimg">
         <?php
@@ -17,40 +25,107 @@ include_once '../Base/requerLogin.php';
         include_once '../Controle/aplicativoPDO.php';
         $Aplicativo = new aplicativoPDO();
             $stmt = $Aplicativo->selectAplicativoId_aplicativo($_GET['id']);
-                $nomeNormal = new Aplicativo($stmt->fetch());
+                $aplicativo = new Aplicativo($stmt->fetch());
         ?>
         <main>
             <div class="row" style="margin-top: 10vh;">
-                <form action="../Controle/aplicativoControle.php?function=editar" class="card col l8 offset-l2 m10 offset-m1 s10 offset-s1" method="post">
+                <form action="../Controle/aplicativoControle.php?function=editar" enctype="multipart/form-data"
+                      class="card col l8 offset-l2 m10 offset-m1 s10 offset-s1" method="post">
+                    <input value="<?php echo $_GET['id'] ?>" name="id_aplicativo" hidden/>
                     <div class="row center">
-                        <h4 class="textoCorPadrao2">Editar Aplicativo</h4>
-                        <div class="input-field col s6" hidden>
-                            <input type="text" name="id_aplicativo" value="<?= $nomeNormal->getId_aplicativo() ?>">
-                            <label>id_aplicativo</label>
+                        <h4 class="textoCorPadrao2">Cadastrar Aplicativo</h4>
+                        <div class="input-field col s6">
+                            <?php
+                                $clientePDO = new ClientePDO();
+                                $stmtCliente = $clientePDO->selectCliente();
+                            ?>
+                            <select id="id_cliente" name="id_cliente">
+                                <?php
+                                    if($stmtCliente){
+                                        while($linha = $stmtCliente->fetch()) {
+                                            $cliente = new Cliente($linha);
+                                            ?>
+                                            <option value='<?php echo $cliente->getIdCliente() ?>' <?php echo $cliente->getIdCliente() == $aplicativo->getIdCliente() ? "seleceted" : "" ?>><?php echo $cliente->getNomeCliente() ?></option>";
+                                            <?php
+                                        }
+                                    }else{
+                                        echo "<option value='0'>Nenhum Cliente</option>";
+                                    }
+
+                                ?>
+                            </select>
+                            <label for="id_cliente">Cliente</label>
                         </div>
                         <div class="input-field col s6">
-                            <input type="text" name="cliente" value="<?= $nomeNormal->getCliente() ?>">
-                            <label>cliente</label>
+                            <?php
+                                $projetoPDO = new ProjetoPDO();
+                                $stmtProjeto = $projetoPDO->selectAll();
+                            ?>
+                            <select id="id_projeto" name="id_projeto">
+                                <option value="0" selected disabled>Selecione</option>
+                                <?php
+                                    if($stmtProjeto){
+                                        while($linha = $stmtProjeto->fetch()) {
+                                            $projeto = new Projeto($linha);
+                                            $versaoPDO = new VersaoPDO();
+                                            $versao = new Versao($versaoPDO->selectId_versao($aplicativo->getIdVersao())->fetch());
+                                            ?>
+                                            <option value='<?php echo $projeto->getIdProjeto() ?>' <?php echo $projeto->getIdProjeto() == $versao->getIdProjeto() ? "selected" : "" ?>><?php echo $projeto->getNomeProjeto() ?></option>";
+                                <?php
+                                        }
+                                    }else{
+                                        echo "<option value='0'>Nenhum Projeto</option>";
+                                    }
+                                ?>
+                            </select>
+
+                            <label for="id_projeto">Projeto</label>
                         </div>
                         <div class="input-field col s6">
-                            <input type="text" name="nome_pacote" value="<?= $nomeNormal->getNome_pacote() ?>">
-                            <label>nome_pacote</label>
+                            <select id="id_versao" name="id_versao">
+                            </select>
+
+                            <label for="id_versao">Versão</label>
                         </div>
                         <div class="input-field col s6">
-                            <input type="text" name="chave" value="<?= $nomeNormal->getChave() ?>">
-                            <label>chave</label>
+                            <?php
+                                $sitePDO = new SitePDO();
+                                $stmtSite = $sitePDO->selectSite();
+                            ?>
+                            <select id="id_site" name="id_site">
+                                <option value="0" selected disabled>Selecione</option>
+                                <?php
+                                    if($stmtSite){
+                                        while($linha = $stmtSite->fetch()) {
+                                            $site = new Site($linha);
+                                            ?>
+                                            <option value='<?php echo $site->getIdSite() ?>'<?php echo $site->getIdSite() == $aplicativo->getIdSite() ? "selected" : "" ?>><?php echo $site->getDominio() ?></option>";
+                                <?php
+                                        }
+                                    }else{
+                                        echo "<option value='0'>Nenhum Site</option>";
+                                    }
+                                ?>
+                            </select>
+
+                            <label for="id_site">Site</label>
                         </div>
                         <div class="input-field col s6">
-                            <input type="text" name="dominio" value="<?= $nomeNormal->getDominio() ?>">
-                            <label>dominio</label>
+                            <input type="text" name="nome_pacote" id="nome_pacote" value="<?php echo $aplicativo->getNomePacote() ?>">
+                            <label for="nome_pacote">Nome do Pacote</label>
                         </div>
                         <div class="input-field col s6">
-                            <input type="text" name="arquivo_firebase" value="<?= $nomeNormal->getArquivo_firebase() ?>">
-                            <label>arquivo_firebase</label>
+                            Chave
+                            <input type="file" name="chave">
                         </div>
-                    <div class="row center">
-                        <a href="../index.php" class="corPadrao3 btn">Voltar</a>
-                        <input type="submit" class="btn corPadrao2" value="Alterar">
+                        <div class="input-field col s6">
+                            Fire Base
+                            <input type="file" name="arquivo_firebase">
+                        </div>
+                        <div class="row center">
+                            <a href="../index.php" class="corPadrao3 btn">Voltar</a>
+                            <input type="submit" class="btn corPadrao2" value="Cadastrar">
+                        </div>
                     </div>
                 </form>
             </div>
@@ -58,6 +133,14 @@ include_once '../Base/requerLogin.php';
         <?php
         include_once '../Base/footer.php';
         ?>
+    <script>
+        $(document).ready(function(){
+            $("#id_versao").load("../Tela/loadSelectVersao.php?id_projeto="+$("#id_projeto").val(), function () {
+                $('select').formSelect();
+            });
+        });
+        $('select').formSelect();
+    </script>
     </body>
 </html>
 
--- Tela/loadSelectVersao.php
+++ Tela/loadSelectVersao.php
@@ -7,7 +7,7 @@ $stmt = $versaoPDO->selectId_projeto($_GET['id_projeto']);
 if($stmt){
     while($linha = $stmt->fetch()){
         $versao = new Versao($linha);
-        echo "<option value='".$versao->getIdVersao()."'>".$versao->getNomeVersao()." / ".$versao->getTextNivel()."</option>";
+        echo "<option value='".$versao->getIdVersao()."' ".($versao->getIdVersao() == $_GET['id_projeto'] ? "selected" : "" ).">".$versao->getNomeVersao()." / ".$versao->getTextNivel()." </option>";
     }
 }else{
     echo "<option>Nenhuma versão!</option>";
